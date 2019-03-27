8ea03bded6c6e8ee654e41f15d18c7288938d495
rmcarpenter <48769319+rmcarpenter@users.noreply.github.com>
2019-03-27 15:38:22 -0500

    Add files via upload

---
+++ Solar_rad.R
@@ -0,0 +1,24 @@
+
+
+solarrad=read.csv("TMY3_StationsMeta.csv")
+names(solarrad)
+
+solarrad$Class=as.numeric(solarrad$Class)
+
+#States
+
+states = solarrad %>% 
+  group_by(State) %>% 
+  summarise(n = n())  # how many sites per state 
+
+
+### map 
+
+r_map <- leaflet(solarrad) %>%
+  addTiles() %>%
+  addCircleMarkers()
+
+r_map
+
+
+
